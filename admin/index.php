409abd7370f964c9627bd9a819d773948a1b1a15
Luca Kiebel <contact@luca-kiebel.de>
2016-06-26 01:45:47 +0200

    admin Folder wouldn't git

---
+++ admin/index.php
@@ -0,0 +1,2 @@
+<?php require_once '../_assets/config.php'; ?>
+
---
+++ admin/install.php
@@ -0,0 +1,14 @@
+<?php require_once '../_assets/config.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require_once '../_assets/includes/head.php';
+    header( "Admin" , "Installation");
+    ?>
+</head>
+<body>
+    <?php include_once '../_assets/includes/admin_install/body.php'; ?>
+</body>
+
+</html>
